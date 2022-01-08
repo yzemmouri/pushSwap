075ae4aa2734642176c4315edbac9faf4aeaaa45
yassine zemmouri <umpzemmouriy@gmail.com>
2022-01-08 01:28:54 +0100

    gg

--- push_swap.c
+++ push_swap.c
@@ -24,7 +24,7 @@ void    three_num(int a, int b, int c, t_indexed_stack *sa, t_indexed_stack *sb)
         manage_instruction_indexed_stack(sa, sb, "sa");
         manage_instruction_indexed_stack(sa, sb, "ra");
     }
-    else
+    else if (a < b && b > c && c < a)
     {
         printf("rra\n");
         manage_instruction_indexed_stack(sa, sb, "rra");
@@ -87,12 +87,12 @@ int main(int ac, char **av)
     // printf("\n\n");
     mergeSort(a.tab, 0, ac - 2, 0);
 
-    // i = 0;
-    // while (i < ac - 1)
-    // {
-    //     printf("%d-->%d\n", a.tab[i].index, a.tab[i].value);
-    //     ++i;
-    // }
+    i = 0;
+    while (i < ac - 1)
+    {
+        printf("%d-->%d\n", a.tab[i].index, a.tab[i].value);
+        ++i;
+    }
     
 
     if (is_sorted_indexed_array(a.tab, ac - 1))
@@ -103,7 +103,11 @@ int main(int ac, char **av)
             three_num(a.tab[2].value, a.tab[1].value, a.tab[0].value, &a, &b);
         else if (ac - 1 <= 5)
         {
-            //manage_instruction_indexed_stack(&a, &b, "pb");
+            if (ac - 1 == 5)
+            {
+                manage_instruction_indexed_stack(&a, &b, "pb");
+                printf("pb\n");
+            }
             manage_instruction_indexed_stack(&a, &b, "pb");
             printf("pb\n");
             three_num(a.tab[2].value, a.tab[1].value, a.tab[0].value, &a, &b);
@@ -128,26 +132,93 @@ int main(int ac, char **av)
                 manage_instruction_indexed_stack(&a, &b, "ra");
                 printf("ra\n");
             }
+            else if (a.tab[a.top].value > a.tab[2].value && a.tab[a.top].value < a.tab[1].value)
+            {    
+                manage_instruction_indexed_stack(&a, &b, "sa");
+                printf("sa\n");
+            }
+            if (ac - 1 == 5)
+            {
+                manage_instruction_indexed_stack(&a, &b, "pa");
+                printf("pa\n");
+                if (a.tab[a.top].value > a.tab[0].value)
+                {
+                    manage_instruction_indexed_stack(&a, &b, "ra");
+                    printf("ra\n");
+                }
+                else if (a.tab[a.top].value > a.tab[1].value && a.tab[a.top].value < a.tab[0].value)
+                {
 
-            manage_instruction_indexed_stack(&a, &b, "pa");
-            printf("pa\n");
+                }
+            }
+            
         }
 
 
         else if (ac - 1 <= 100)
         {
-           
+            int i;
+            int len_chunk = 2;
+
+            int min = 0;
+            int max = (a.top / 5);
+
+            int up = a.top;
+            int down = 0;
+
+            int chunk_num = 1;
+
+            int middle = a.top / 2;
+
+            while (down <= middle)
+            {
+                if (a.tab[down].value >= min && a.tab[down].value <= max)
+                {
+                    printf("**%d**",down);
+                    break;
+                }
+                down++;
+            }
+            while (up > middle)
+            {
+                if (a.tab[up].value >= min && a.tab[up].value <= max)
+                {
+                    printf("++%d++",up);
+                    break;
+                }
+                up--;
+            }
+            if (down + 1 < a.top - up)
+            {
+                printf("kkk");
+                i = down + 1;
+                while (i > 0)
+                {
+                    manage_instruction_indexed_stack(&a, &b, "rra");
+                    i--;
+                }
+            }
+            else
+            {
+                i = a.top - up;
+                while (i > 0)
+                {
+                    printf("fef");
+                    manage_instruction_indexed_stack(&a, &b, "ra");
+                    i--;
+                }
+            }
         }
-        else if (ac - 1 <= 500)
-        {
+        // else if (ac - 1 <= 500)
+        // {
             
-        }
+        // }
     }
-    while (a.top >= 0)
-    {
-        printf("%d ", a.tab[a.top].value);
-        --a.top;
-    }
+    // while (a.top >= 0)
+    // {
+    //     printf("%d ", a.tab[a.top].value);
+    //     --a.top;
+    // }
 
     printf("\n");
     while (b.top >= 0)
